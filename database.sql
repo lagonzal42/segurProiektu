c29ec61b44b66cca04838bd398ab909aeffa9f0d
lagonzal <larraingonzalez@gmail.com>
2025-10-23 09:57:50 +0200

    Babarrunak gehituta sql scriptean datu basea populatzeko

--- database.sql
+++ database.sql
@@ -92,6 +92,33 @@ INSERT INTO `usuarios` VALUES
 (1,'mikel'),
 (2,'aitor');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
+INSERT INTO `babarrunak` (`id`, `Izena`, `Jatorria`, `Kolorea`, `Egozketa_denb_min`) VALUES
+  (1, 'Tolosa', 'Euskal Herria', 'beltza', 90),
+  (2, 'Verdina', 'Asturias', 'berdea', 70),
+  (3, 'Pintoa', 'Castilla y León', 'marroixka', 75),
+  (4, 'Canela', 'La Rioja', 'horia', 80),
+  (5, 'Carilla', 'Andalucía', 'zuria', 60),
+  (6, 'Fabada', 'Asturias', 'zuria', 90),
+  (7, 'Negra de Burgos', 'Castilla y León', 'beltza', 85),
+  (8, 'Alubia de Gernika', 'Bizkaia', 'beltzarana', 80),
+  (9, 'Azuki', 'Japon', 'gorria', 45),
+  (10, 'Manteca', 'Navarra', 'horia', 65),
+  (11, 'Blanca riñón', 'Cantabria', 'zuria', 70),
+  (12, 'Plancheta', 'Catalunya', 'zuria', 60),
+  (13, 'Alubia morada', 'Galicia', 'morea', 75),
+  (14, 'Cannellini', 'Italia', 'zuria', 65),
+  (15, 'Frijol negro', 'México', 'beltza', 55);
+
+
+/*CREATE TABLE `babarrunak` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Izena` varchar(30) NOT NULL,
+  `Jatorria` varchar(30) DEFAULT NULL,
+  `Kolorea` varchar(20) DEFAULT NULL,
+  `Egozketa_denb_min` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;*/
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
