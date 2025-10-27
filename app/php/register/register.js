bf3ae1faf513c1f947eb7a256dd2bccc20e5d87c
Asier Barrio Borge <asierbarrioborge10@gmail.com>
2025-10-27 09:38:27 +0100

    Register amaituta

--- app/php/register/register.js
+++ app/php/register/register.js
@@ -3,20 +3,23 @@ function datuakegiaztatu() {
     var tlnf = document.querySelector('input[name="tlnf"]').value;
     var jaiodata = document.querySelector('input[name="jaiodata"]').value;
     var mail = document.querySelector('input[name="mail"]').value;
+    var izen = document.querySelector('input[name="iz_abz"]').value;
+    var pas = document.querySelector('input[name="pas"]').value;
 
     var nanRegex = /^[0-9]{8}[A-Z]$/;
     var mailRegex = /^[A-Za-z0-9.]+@{1}[A-Za-z0-9.]+.[A-Za-z]{2,}$/;
     var jaioRegex = /^\d{4}-\d{2}-\d{2}$/;
+    var izenRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+
 
     if (nan.length < 9 || nan.length > 9) {
-        window.alert("NAN-aren luzera ez da egokia.")
+        window.alert("NA-aren luzera ez da egokia.")
         return false;
     }
     else if (!nanRegex.test(nan)) {
         window.alert("NAN-ak ez du formatu egokia.")
         return false;
     }
-
 
     if (tlnf.length < 9 || tlnf.length > 9) {
         window.alert("Telefono zenbakiaren luzera ez da egokia.")
@@ -30,6 +33,17 @@ function datuakegiaztatu() {
     
     if(!jaioRegex.test(jaiodata)){
         window.alert("Jaiotze data ez da egokia.")
+        return false;
+    }
+    
+    if(!izenRegex.test(izen)){
+        window.alert("Izen hori ez da onartzen")
+        console.log(pas)
+        return false;
+    }
+
+    if (pas.trim() === "") {
+        window.alert("Pasahitza ezin da hutsik egon.");
         return false;
     }
 
