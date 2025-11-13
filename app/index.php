c171f416217f9382bbee0ad166df80191b855af5
Asier Barrio Borge <asierbarrioborge10@gmail.com>
2025-11-13 09:26:28 +0100

    Header-ak gehitzen amaituta

--- app/index.php
+++ app/index.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+
+header_remove("X-Powered-By");
+header("Server: SegurServer");
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: DENY");
+header("X-XSS-Protection: 1; mode=block");
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
